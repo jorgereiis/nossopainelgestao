bb552c646b79e264d8b89d1867c24fdce91f9e69
Jorge Reis <jreiisgalvao@gmail.com>
2023-05-05 20:48:22 -0300

    Atualizando

--- cadastros/signals.py
+++ cadastros/signals.py
@@ -6,20 +6,20 @@ from dateutil.relativedelta import relativedelta
 from .models import Mensalidade, Cliente, Plano, definir_dia_pagamento
 
 # funcão para definir o dia de pagamento
-def definir_dia_renovacao(dia_adesao):
-    if dia_adesao in range(5, 10):
+def definir_dia_renovacao(dia):
+    if dia in range(5, 10):
         dia_pagamento = 5
-    elif dia_adesao in range(10, 15):
+    elif dia in range(10, 15):
         dia_pagamento = 10
-    elif dia_adesao in range(15, 20):
+    elif dia in range(15, 20):
         dia_pagamento = 15
-    elif dia_adesao in range(20, 25):
+    elif dia in range(20, 25):
         dia_pagamento = 20
-    elif dia_adesao in range(25, 30):
+    elif dia in range(25, 30):
         dia_pagamento = 25
     else:
-        dia_pagamento = 30
-    return dia_pagamento
+        dia = 30
+    return dia
 
 # Atualiza o `ultimo_pagamento` do cliente da mensalidade
 @receiver(post_save, sender=Mensalidade)
@@ -87,7 +87,7 @@ def criar_nova_mensalidade(sender, instance, **kwargs):
             )
         # Se não, define a variável tendo um nova data baseada no `dia_atual`
         else:
-            novo_dia_de_pagamento = definir_dia_renovacao(dia_atual)
+            novo_dia_de_pagamento = definir_dia_pagamento(dia_atual)
             novo_vencimento = datetime(
                 instance.dt_pagamento.year,
                 instance.dt_pagamento.month,
