4f26209e35fe88bb5a11803302d048582dbb7897
Jorge Reis <jreiisgalvao@gmail.com>
2023-07-06 12:03:58 -0300

    Ajustes do script de envio automático

--- enviar_mensagens.py
+++ enviar_mensagens.py
@@ -180,7 +180,7 @@ def mensalidades_vencidas():
 
 # Agendar a execução das funções
 schedule.every().day.at("10:00").do(mensalidades_a_vencer)
-schedule.every().day.at("12:00").do(mensalidades_vencidas)
+schedule.every().day.at("10:00").do(mensalidades_vencidas)
 
 # Executar indefinidamente
 while True:
