b67bc8cf29f9576a91992c43ac5436f03a6026a4
Jorge Reis <jreiisgalvao@gmail.com>
2024-10-26 12:35:41 -0300

    Up

--- enviar_mensagens.py
+++ enviar_mensagens.py
@@ -617,13 +617,13 @@ def run_threaded(job):
 
 
 ##### Agendar a execução das tarefas
-schedule.every().day.at("12:00").do(
+schedule.every().day.at("10:00").do(
     run_threaded, run_scheduled_tasks
 )
-schedule.every().day.at("11:30").do(
+schedule.every().day.at("11:00").do(
     run_threaded, mensalidades_a_vencer
 )
-schedule.every().day.at("12:30").do(
+schedule.every().day.at("11:30").do(
     run_threaded, mensalidades_vencidas
 )
 schedule.every().day.at("17:00").do(
