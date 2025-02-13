602b29a876e4dc51a942142198f2030d90a6e5e2
Jorge Reis <jreiisgalvao@gmail.com>
2025-02-13 11:54:22 -0300

    Ajustando core.settings para deploy público

--- setup/settings.py
+++ setup/settings.py
@@ -38,9 +38,9 @@ SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
 RECAPTCHA_REQUIRED_SCORE = 0.85
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['meusistema.com.br','nossopainel.com.br', 'www.nossopainel.com.br', 'localhost', '127.0.0.1']
 
 # Application definition
 
