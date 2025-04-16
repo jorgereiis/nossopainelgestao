ea981f66a7518ab2c8e25c25eded9bb929f7bbe7
Jorge Reis <jreiisgalvao@gmail.com>
2025-04-16 16:18:21 -0300

    Up

--- cadastros/models.py
+++ cadastros/models.py
@@ -5,6 +5,38 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 
 
+# Dicionário de DDDs por UF para definir o estado do cliente
+DDD_UF_MAP = {
+    '11': 'SP', '12': 'SP', '13': 'SP', '14': 'SP', '15': 'SP', '16': 'SP', '17': 'SP', '18': 'SP', '19': 'SP',
+    '21': 'RJ', '22': 'RJ', '24': 'RJ',
+    '27': 'ES', '28': 'ES',
+    '31': 'MG', '32': 'MG', '33': 'MG', '34': 'MG', '35': 'MG', '37': 'MG', '38': 'MG',
+    '41': 'PR', '42': 'PR', '43': 'PR', '44': 'PR', '45': 'PR', '46': 'PR',
+    '47': 'SC', '48': 'SC', '49': 'SC',
+    '51': 'RS', '53': 'RS', '54': 'RS', '55': 'RS',
+    '61': 'DF',
+    '62': 'GO', '64': 'GO',
+    '63': 'TO',
+    '65': 'MT', '66': 'MT',
+    '67': 'MS',
+    '68': 'AC',
+    '69': 'RO',
+    '71': 'BA', '73': 'BA', '74': 'BA', '75': 'BA', '77': 'BA',
+    '79': 'SE',
+    '81': 'PE', '87': 'PE',
+    '82': 'AL',
+    '83': 'PB',
+    '84': 'RN',
+    '85': 'CE', '88': 'CE',
+    '86': 'PI', '89': 'PI',
+    '91': 'PA', '93': 'PA', '94': 'PA',
+    '92': 'AM', '97': 'AM',
+    '95': 'RR',
+    '96': 'AP',
+    '98': 'MA', '99': 'MA',
+}
+
+
 # funcão para definir o dia de pagamento
 def definir_dia_pagamento(dia_adesao):
     if dia_adesao in range(3, 8):
@@ -150,6 +182,7 @@ class Cliente(models.Model):
             self.data_pagamento = dia
 
         self.formatar_telefone()
+        self.definir_uf()
 
         super().save(*args, **kwargs)
 
@@ -176,6 +209,13 @@ class Cliente(models.Model):
                     self.telefone = f'({ddd}) {numero[:4]}-{numero[4:]}'  # Formato (DD) DDDD-DDDD
                 else:
                     self.telefone = f'({ddd}) 9{numero[:4]}-{numero[4:]}'  # Formato (DD) DDDDD-DDDD
+
+    def definir_uf(self):
+        raw_telefone = re.sub(r'\D+', '', self.telefone)
+        if raw_telefone.startswith('55'):
+            raw_telefone = raw_telefone[2:]
+        ddd = raw_telefone[:2]
+        self.uf = DDD_UF_MAP.get(ddd, None)
 
     def __str__(self):
         return self.nome
