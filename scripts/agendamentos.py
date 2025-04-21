b81e844ed927aa2f6be70c32ea685c89f78391fd
Jorge Reis <jreiisgalvao@gmail.com>
2025-04-21 15:49:18 -0300

    Up

--- scripts/agendamentos.py
+++ scripts/agendamentos.py
@@ -36,7 +36,7 @@ def run_threaded(job):
     job_thread.start()
 
 # Agendar a execução das tarefas
-schedule.every().day.at("12:53").do(
+schedule.every().day.at("12:00").do(
     run_threaded, run_scheduled_tasks
 )
 schedule.every().day.at("13:00").do(
