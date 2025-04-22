5c111ae5fb75234419b8bff043d92c9f8a308a9e
Jorge Reis <jreiisgalvao@gmail.com>
2025-04-22 00:19:41 -0300

    Up

--- scripts/agendamentos.py
+++ scripts/agendamentos.py
@@ -48,13 +48,13 @@ schedule.every().day.at("13:30").do(
 schedule.every().day.at("17:00").do(
     run_threaded, mensalidades_canceladas
 )
-schedule.every().day.at("00:10").do(
+schedule.every().day.at("00:30").do(
     run_threaded, executar_comparar_lista_m3u8_com_lock
 )
-schedule.every().day.at("00:20").do(
+schedule.every().day.at("00:40").do(
     run_threaded, processar_novos_titulos_com_lock
 )
-schedule.every().day.at("00:30").do(
+schedule.every().day.at("00:50").do(
     run_threaded, executar_upload_status_com_lock
 )
 schedule.every(60).minutes.do(
