4ad2bc73da5ff623b63cc1200ff31305fbc7506c
Jorge Reis <jreiisgalvao@gmail.com>
2025-06-21 02:53:38 -0300

    Melhoria dos envios agendados

--- scripts/agendamentos.py
+++ scripts/agendamentos.py
@@ -36,9 +36,9 @@ def run_threaded(job):
     job_thread.start()
 
 # Agendar a execução das tarefas em horários específicos
-"""schedule.every().day.at("12:00").do(
+schedule.every().day.at("12:00").do(
     run_threaded, run_scheduled_tasks
-)"""
+)
 schedule.every().day.at("17:00").do(
     run_threaded, obter_mensalidades_canceladas
 )
